4d0a77b18f5d5a63dd66ea75dade8f3104426614
SaidBenaissa <d.sbenaissa@gmail.com>
2020-02-18 16:50:57 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let socket = require('socket.io');
+// App 
+let app = express();
+
+// Server
+let server = app.listen(4000, () =>{
+         console.log("Listening to port 4000");
+});
+
+// Static files
+app.use(express.static('public'));
+
+// Socket SetUp : take as input the server to connect with
+let io = socket(server);
+io.on('connection', (socket) => {
+    console.log(' Sockets connection maded ')
+});
